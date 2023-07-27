459f9ccd2404a1a3c1ec0295efcc1210cd94548e
Alex Mann <amann@physna.com>
2023-07-27 14:22:15 -0400

    under construction personal site

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,15 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Alex Mann</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="This site is under construction (obviously)" />
+        <a href="https://quiet-jelly-feeb71.netlify.app/">
+          ETA-Checker
+        </a>
       </main>
 
       <Footer />
